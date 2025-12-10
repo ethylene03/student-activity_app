4e89a7208bac861bf0e27b1122c302e51d08c20e
Princess Ethel Atillo <peatillo@vauldex.com>
2025-12-10 13:47:19 +0800

    feat: create homepage

---
+++ src/views/HomePage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useAuthorizationStore } from '@/stores/authorization';
+
+const auth = useAuthorizationStore()
+</script>
+
+<template>
+  <section id="home-page" class="section">
+    <h1>Welcome, {{ auth.firstName }}!</h1>
+    <p class="lead">View your summary of activities here.</p>
+  </section>
+</template>
