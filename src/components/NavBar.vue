121e279b7aa08d85e68f934f6fa900fe216159b9
Princess Ethel Atillo <peatillo@vauldex.com>
2025-12-10 13:45:04 +0800

    initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,101 @@
+<script setup lang="ts">
+import { logout } from '@/api/authorization'
+import { useAuthorizationStore } from '@/stores/authorization'
+import { ArrowRightStartOnRectangleIcon } from '@heroicons/vue/24/solid'
+import { computed, ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+const auth = useAuthorizationStore()
+
+async function logoutStudent() {
+  await logout()
+
+  auth.$reset()
+
+  router.push({ name: 'Login' })
+}
+
+const currentPath = computed(() => {
+  return route.name?.toString().toLowerCase() || ''
+})
+
+const isOpen = ref<boolean>(false)
+const showDropdown = ref<boolean>(false)
+
+watch(
+  () => route.path,
+  () => {
+    isOpen.value = false
+    showDropdown.value = false
+  },
+)
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-white shadow-sm position-sticky top-0 z-3">
+    <div class="container-fluid py-3 px-5">
+      <h1 class="navbar-brand mb-0">Project Name</h1>
+      <button
+        class="navbar-toggler"
+        type="button"
+        aria-label="Toggle navigation"
+        @click="isOpen = !isOpen"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div
+        id="navbarNav"
+        :class="[{ collapse: !isOpen }, 'navbar-collapse', { show: isOpen }]"
+        class="ms-3 justify-content-between"
+      >
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/home">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/activities">Activities</router-link>
+          </li>
+        </ul>
+
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <button
+              v-if="currentPath !== 'login' && currentPath !== 'signup'"
+              class="nav-link"
+              @click="logoutStudent"
+            >
+              <arrow-right-start-on-rectangle-icon
+                class="me-2"
+                style="width: 1.25rem; height: 1.25rem"
+              />
+              Logout
+            </button>
+            <button
+              v-if="currentPath === 'signup'"
+              class="nav-link"
+              @click="$router.push({ name: 'Login' })"
+            >
+              Login
+            </button>
+            <button
+              v-if="currentPath === 'login'"
+              class="nav-link"
+              @click="$router.push({ name: 'Signup' })"
+            >
+              Signup
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.router-link-active {
+  color: var(--bs-primary);
+}
+</style>
