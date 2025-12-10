ae3fa382b845be12b1f3a7e2a41da20b8f5ba4cb
Princess Ethel Atillo <peatillo@vauldex.com>
2025-12-10 17:25:00 +0800

    fix: remove time in date format

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -18,8 +18,6 @@ function formatDate(date?: string): string {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
   }
   return new Date(date || '').toLocaleDateString('en-PH', options)
 }
